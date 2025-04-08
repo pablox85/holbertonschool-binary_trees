0ea74e4700e12ab4172818f6cf91ed315624100a
pablox85 <pdpcorrales@gmail.com>
2025-04-08 18:16:58 +0000

    New Nod

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_node - nuevo nodo al arbol
+ * @parent: nodo padre
+ * @value: valor a almacenar
+ * Return: al nodo nuevo
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *nN;
+
+	nN = malloc(sizeof(binary_tree_t));
+
+	if (nN == NULL)
+	{
+	return (NULL);
+	}
+
+	nN->n = value;
+	nN->parent = parent;
+	nN->left = NULL;
+	nN->right = NULL;
+
+	return (nN);
+}
