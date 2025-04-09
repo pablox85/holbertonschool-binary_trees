02fed2e23c943cdb814ee41327cf5df9d4afb21a
pablox85 <pdpcorrales@gmail.com>
2025-04-09 17:22:20 +0000

    is leaf (?) updt

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,14 +9,18 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)
-		return (0);
-
 	if (node->parent == NULL)
+	{
 		return (1);
-
-	if (node->parent != NULL)
+	}
+	else if (node->parent != NULL)
+	{
 		return (0);
+	}
+	else
+	{
+		return (0);
+	}
 
-	return (0);
+	
 }
