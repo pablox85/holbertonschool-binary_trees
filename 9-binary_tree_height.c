7820932b0c0026ce1c7b553fd2f523675c0f3b89
pablox85 <pdpcorrales@gmail.com>
2025-04-09 20:44:55 +0000

    hieght

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_height - altura del arbol
+ * @tree: prt al arbol
+ * Retunr: 0
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+size_t  leftH, rightH, height;
+
+	if (tree == NULL)
+		return (0);
+
+	leftH = binary_tree_height(tree->left);
+	rightH = binary_tree_height(tree->right);
+
+	if (leftH > rightH)
+	{
+		height = leftH;
+	}
+	else
+	{
+		height = rightH;
+	}
+
+	return(height +1);
+}
+
