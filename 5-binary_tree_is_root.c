6d951b284dd3f7aef79b781a982fac27eb6bed6d
pablox85 <pdpcorrales@gmail.com>
2025-04-09 17:05:34 +0000

    check if parent == root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - chekea si el nodo es root
+ * @node: nodo dado
+ * Return: algo
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent != NULL)
+		return (1);
+
+	return(0);
+}
