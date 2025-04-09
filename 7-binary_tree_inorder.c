2f94b88029661a7b6134c0852b1908652e426a73
pablox85 <pdpcorrales@gmail.com>
2025-04-09 18:33:11 +0000

    tree in order from left->node->right

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * void binary_tree_inorder - recorrer transversalmente
+ * @tree: ptr al arbol
+ * @func: ptr a la funcion
+ * Return: void
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
